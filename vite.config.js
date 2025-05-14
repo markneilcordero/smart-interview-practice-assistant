6f78ea45fc046d5dc5ef706a11e630b3489a8ecb
Mark Neil <mark.neil.u.cordero@gmail.com>
2025-05-14 12:27:41 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // 👈 this fixes 404 on refresh
-  },
+  // server: {
+  //   historyApiFallback: true, // 👈 this fixes 404 on refresh
+  // },
 })
